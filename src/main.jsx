c7ab1a0893df9af3291e0ee5bc83a0a00a56fbc4
Sparsh Goyal <sparshg05@gmail.com>
2025-03-20 16:04:31 +0530

    frontend deployment

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import 'remixicon/fonts/remixicon.css';
+
+createRoot(document.getElementById('root')).render(
+    <App />
+)
